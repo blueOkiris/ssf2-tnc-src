a66f9aa28f60a43eb0b5343813e4c2e0e1011e2c
Dylan Turner <dylantdmt@gmail.com>
2023-02-10 16:35:10 -0600

    main: created basic entry point
    Signed-off-by: Dylan Turner <dylantdmt@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Author(s): Dylan Turner <dylantdmt@gmail.com>
+ * Description: Entry point for Super Street Fighter II - The New Challengers
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **args) {
+    printf("Hello, world!\n");
+    return 0;
+}
+
